20a2b47e19279fd6580ed43280c1ba072027e009
B-Aishwarya <72915098+B-Aishwarya@users.noreply.github.com>
2020-12-29 00:06:53 +0530

    Update FlightInfoTester.java

--- execusion/src/com/xworkz/flightBooking/flightBookingDetailes/FlightInfoTester.java
+++ execusion/src/com/xworkz/flightBooking/flightBookingDetailes/FlightInfoTester.java
@@ -8,11 +8,21 @@ import com.xworkz.flightBooking.bookingInfo.BookingInfo;
 public class FlightInfoTester {
 	public static void main(String[] args) {
 		BookingInfo bookingInfo = new BookingInfo(50);
-		FlightInfoDTO dto = new FlightInfoDTO("AIR234", 1987, "udupi", Company.INDIGO);
+		FlightInfoDTO dto = new FlightInfoDTO("AIR234", 1987, "bangalore", Company.INDIGO);
 		bookingInfo.acceptBookings(dto);
 		bookingInfo.displayBookingInfo();
-		bookingInfo.displayBookingAtStartIndex("udupi");
+
+		FlightInfoDTO dto1 = new FlightInfoDTO("AIR897", 1767, "delhi", Company.SPICEJET);
+		bookingInfo.acceptBookings(dto1);
+		bookingInfo.displayBookingInfo();
+		bookingInfo.displayBookingAtStartIndex("delhi");
+
+		FlightInfoDTO dto2 = new FlightInfoDTO("AIRIND6578", 967, "delhi", Company.AIRINDIA);
+		bookingInfo.acceptBookings(dto2);
+		bookingInfo.displayBookingInfo();
+		bookingInfo.displayBookingAtStartIndex("aish");
 
 	}
 
 }
+
